dec37824e126a40231cb95ddeba3c0faac4ce601
Gordon Bateson <gordonbateson@gmail.com>
2018-02-08 09:57:18 +0900

    mod/englishcentral use png image file instead of data URI to show play icon on video thumbnails

--- version.php
+++ version.php
@@ -32,5 +32,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'mod_englishcentral';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2015051100; // Moodle 2.9
-$plugin->version   = 2018020718;
-$plugin->release   = '2018-02-07 (18)';
+$plugin->version   = 2018020919;
+$plugin->release   = '2018-02-09 (19)';
