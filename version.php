e69520d9862d0f64e771699c651873c496f8266c
Gordon Bateson <gordonbateson@gmail.com>
2022-03-17 14:17:51 +0900

    mod_englishcentral fix layout of goal settings on Boost-based themes

--- version.php
+++ version.php
@@ -32,5 +32,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'mod_englishcentral';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2015051100; // Moodle 2.9 (because we want to use AMD)
-$plugin->version   = 2021081800;
-$plugin->release   = '2021-08-18 (22)';
+$plugin->version   = 2022031719;
+$plugin->release   = '2022-03-17 (19)';
