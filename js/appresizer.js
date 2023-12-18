e62ee42af9d1e6c07829df9a8badf358ef658712
justinhunt <bitwalkerjapan@gmail.com>
2023-12-18 06:26:20 +0000

    still not working

--- js/appresizer.js
+++ js/appresizer.js
@@ -1,13 +1,12 @@
 //EC iframe Resizer
 (function () {
      if ( !window.addEventListener ) {
-        console.log("appresizer.js cant run without a window");
+        console.log("appresizer.js cant run yet");
         return; // Not supported
     }
     console.log("appresizer.js initializing");
-    window.ecappResizerInitialized = true;
-    // Scroll the page content 120 pixels higher
-    // Function to scroll the specified element by its own height
+
+    // Scroll the page content higher
     function scrollElementUpByHeight() {
         console.log("appresizer.js scrolling");
         document.querySelector('ion-content:not(.disable-scroll-y)').scrollTo({top: 80});
@@ -16,10 +15,12 @@
     // Attach the event listener to the 'load' event of the iframe
     window.addEventListener('load', function(e){
         console.log("appresizer.js windowload");
+        //try it again
         document.getElementById('englishcentral-mobileapp-iframe').addEventListener('load', scrollElementUpByHeight);
-        //just in case
+        //try it again
         scrollElementUpByHeight();
     });
+    //try it once
     scrollElementUpByHeight();
 
 
