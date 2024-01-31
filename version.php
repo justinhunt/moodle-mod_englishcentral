db9105b27410eda649c33eca13cd7111134fdfe4
justinhunt <bitwalkerjapan@gmail.com>
2024-01-31 04:53:40 +0000

    version bump for better mobile app sizing

--- version.php
+++ version.php
@@ -32,5 +32,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'mod_englishcentral';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2015051100; // Moodle 2.9 (because we want to use AMD)
-$plugin->version   = 2023111237;
-$plugin->release   = '2023-11-12 (37)';
+$plugin->version   = 2024013138;
+$plugin->release   = '2024-01-31 (38)';
