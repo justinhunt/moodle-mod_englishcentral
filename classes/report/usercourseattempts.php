b122e14e0f9c831f176c2e18b947cccedd2c90eb
Justin Hunt <bitwalkerjapan@gmail.com>
2024-12-30 15:59:29 +0900

    set yaxiz size

--- classes/report/usercourseattempts.php
+++ classes/report/usercourseattempts.php
@@ -136,8 +136,8 @@ class usercourseattempts extends basereport {
         $chart = new \core\chart_bar();
         $chart->set_horizontal(false);
         $chart->set_stacked(false);
+    //    $yzeroaxis = $chart->get_yaxis(0, true);
         $yaxis = $chart->get_yaxis(0, true);
-        $yaxis = $chart->get_yaxis(1, true);
         $yaxis->set_stepsize(10);
         $yaxis->set_min(0);
         $yaxis->set_max(100);
