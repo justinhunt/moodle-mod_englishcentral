71d9aac0163c1eabd9dcc69231b44fc41be9fc19
Justin Hunt <bitwalkerjapan@gmail.com>
2025-06-24 09:31:24 +0900

    version bump

--- version.php
+++ version.php
@@ -32,5 +32,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'mod_englishcentral';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2015051100; // Moodle 2.9 (because we want to use AMD)
-$plugin->version   = 2025050145;
-$plugin->release   = '2025-05-01 (45)';
+$plugin->version   = 2025062446;
+$plugin->release   = '2025-06-24 (46)';
